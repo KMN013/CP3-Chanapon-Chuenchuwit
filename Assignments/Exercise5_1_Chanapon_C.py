f0f5735ce9f1c362fffedea87eee0d7a8082df81
KMN013 <kong-1312@hotmail.co.th>
2020-03-23 15:22:21 +0700

    Delete Exercise5_1_Chanapon_C.py

--- Assignments/Exercise5_1_Chanapon_C.py
+++
@@ -1,7 +0,0 @@
-'''โปรแกรมคำนวณเลข 2 ตัว '''
-num1 = int(input("ตัวที่ 1 :"  ))
-num2 = int(input("ตัวที่ 2 :"  ))
-print(num1,"+",num2,"=",num1+num2)
-print(num1,"-",num2,"=",num1-num2)
-print(num1,"*",num2,"=",num1*num2)
-print(num1,"/",num2,"=",float(num1/num2))
